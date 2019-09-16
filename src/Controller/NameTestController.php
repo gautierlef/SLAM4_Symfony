4996771fb286da934e6a19e4936c8c1ac8a26b16
etudiant <etudiant@debian9-gui.sio.lla.fr>
2019-09-16 15:50:40 +0200

    Projet_test_findAll()_fonctionnel

--- src/Controller/NameTestController.php
+++ src/Controller/NameTestController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
-
+use App\Entity\Testtable;
 
 class NameTestController extends AbstractController
 {
@@ -14,9 +14,51 @@ class NameTestController extends AbstractController
      */
     public function index()
     {
-        return new Response("<h1 style=\"text-decoration: underline;\">Bonjour tout le monde!<h1/>");
-/*        return $this->render('name_test/index.html.twig', [
-            'controller_name' => 'NameTestController',
-        ]);*/
+        $entityManager = $this->getDoctrine()->getManager();
+        $repository = $entityManager->getRepository(Testtable::class);
+        $lesTests = $repository->findAll();
+        if ($lesTests == null) {
+            $this->setTests();
+        }
+        return $this->render('name_test/index.html.twig', [
+            'resultats' => $this->getTests(),
+        ]);
+    }
+    
+    public function setTests() {
+/*        $entityManager = $this->getDoctrine()->getManager();
+        $repository = $entityManager->getRepository(Testtable::class);
+        $lesAnciensTests = $repository->findAll();
+        foreach ($lesAnciensTests as $ancienTest) {
+           $entityManager->remove($ancienTest);
+        }
+        $entityManager->flush();*/
+        
+        $test = new Testtable();
+        $test->setNom("Leblond");
+        $test->setPrenom("Paul");
+        $test->setAdresse("La Creuse");
+        $test->setAge(23);
+        $entityManager->persist($test);
+        $entityManager->flush();
+        $test1 = new Testtable();
+        $test1->setNom("Lechatain");
+        $test1->setPrenom("Gérard");
+        $test1->setAdresse("Pays Basques");
+        $test1->setAge(18);
+        $entityManager->persist($test1);
+        $entityManager->flush();
+        $test2 = new Testtable();
+        $test2->setNom("Leroux");
+        $test2->setPrenom("Nicolas");
+        $test2->setAdresse("Paris");
+        $test2->setAge(35);
+        $entityManager->persist($test2);
+        $entityManager->flush();
+    }
+    
+    public function getTests() {
+        $lesTests = $this->getDoctrine()->getRepository(Testtable::class)->findAll();
+        return $lesTests;
     }
 }
